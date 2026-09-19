18ceb012a1cc2b160d7cfa4c8636fb6f5c58cb8d
Yuto Tagashira <ut0s.dev@gmail.com>
2026-09-20 00:33:20 +0900

    fix(cask): remove deprecated verified parameter and interpolate version in url

--- Casks/alttabalt.rb
+++ Casks/alttabalt.rb
@@ -2,8 +2,7 @@ cask "alttabalt" do
   version "1.0.11"
   sha256 "f4a7343579ffcd36968626d2b1d8d04ed76fd7c6485a6db195c65eadda0496ef"
 
-  url "https://github.com/ut0s/homebrew-alttabalt/releases/download/v1.0.11/AltTabAlt-v1.0.11.zip",
-      verified: "github.com/ut0s/homebrew-alttabalt/"
+  url "https://github.com/ut0s/homebrew-alttabalt/releases/download/v#{version}/AltTabAlt-v#{version}.zip"
   name "AltTabAlt"
   desc "Window switcher with Alt-Tab behavior"
   homepage "https://github.com/ut0s/homebrew-alttabalt"
